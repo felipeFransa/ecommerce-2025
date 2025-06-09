15f4c8f49cf76463ae5e8e45906b2389179cbf91
Felipe Fransa <antoniofelipesilva@live.com>
2025-06-08 23:16:42 -0300

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner reading = new Scanner(System.in);
         String welcomeFirst = "Welcome";
         System.out.println(welcomeFirst);
     }
