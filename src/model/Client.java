f1583d9a9ee4b464936d4dba9bea6c5b28e65234
Felipe Fransa <antoniofelipesilva@live.com>
2025-06-13 18:43:34 -0300

    model client

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Client {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
